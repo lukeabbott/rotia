7fc08267995ce884b55362504ff78d5ffabe5729
Luke Abbott <labbott@corp.woodlanddirect.com>
2018-02-21 16:32:31 -0500

    Make the mouse run

--- src/rodent.js
+++ src/rodent.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 class Rodent extends React.Component {
+  constructor (props) {
+    super(props)
+    this.mouse = '~~~<=>o-'
+    this.mouse1 = '~~~>=<o-'
+    this.state = {
+      step: 0
+    }
+  }
+  step () {
+    this.setState(function (previousState, props) {
+      return {step: previousState.step === 0 ? 1 : 0}
+    })
+  }
   render () {
     return (
-      <div>{'~~~<=>o-'}</div>
+      <div onClick={this.step.bind(this)} >{
+        this.state.step === 0 ? this.mouse : this.mouse1
+        }
+      </div>
     )
   }
 }
