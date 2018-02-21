7c898a2de27807d05e27ddd316041a554d5a2ee9
Luke Abbott <labbott@corp.woodlanddirect.com>
2018-02-21 15:10:04 -0500

    Webpack 4, Babel 7

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [ '@babel/preset-env', {
+                'targets': {
+                  'browsers': ['last 2 versions', 'safari >= 7']
+                }
+              }],
+              '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
