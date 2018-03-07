1fa213e83c5e6751cd28bfb88ac95c219183af03
Luke Abbott <labbott@corp.woodlanddirect.com>
2018-03-07 16:43:12 -0500

    svg for rodent

---
+++ src/critters/spider.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import BaseStats from './baseStats'
+
+const Spider = () => {
+  <svg width='50' height='20' x={this.state.position.x} y={this.state.position.y}>
+    <rect width='20' height='5' x='0' y='10' fill={this.look.color} />
+    <rect width='15' height='15' x='20' y='3' fill={this.look.color} />
+    <rect width='5' height='20' x='35' y='0' fill={this.look.color} />
+    <rect width='10' height='5' x='40' y='10' fill={this.look.color} />
+  </svg>
+}
+
+export default Spider
